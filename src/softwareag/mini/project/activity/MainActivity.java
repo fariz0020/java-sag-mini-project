3ec0aec860f33b07e08f9ad30c28a182bb75f679
fariznurzam <t90fariz@gmail.com>
2018-12-14 13:35:46 +0700

    add model & activities

--- src/softwareag/mini/project/activity/MainActivity.java
+++ src/softwareag/mini/project/activity/MainActivity.java
@@ -21,15 +21,31 @@ public class MainActivity {
     }
     
     public void welcomeActivity() {
+        boolean loop = false;
         System.out.println("Welcome to IMan APP - Inventory Management Application");
         System.out.println("Please choose the option");
-        System.out.println("1. Manage Products");
-        System.out.println("2. Manage Categories");
-        System.out.println("3. Manage Purchases");
-        System.out.println("4. Manage Sales");
-        
-        int option = scanner.nextInt();
-        System.out.println("You choose " + option);
+        while (loop == false) {
+            System.out.println("1. Manage Products");
+            System.out.println("2. Manage Categories");
+            System.out.println("3. Manage Purchases");
+            System.out.println("4. Manage Sales");
+            System.out.println("0. Exit Application");
+            System.out.println("Write : ");
+            String option = scanner.nextLine();
+            if (option == "1") {
+                
+            } else if (option == "2") {
+                
+            } else if (option == "3") {
+                
+            } else if (option == "4") {
+                
+            } else if (option == "0") {
+                System.out.println("Thank you for using the application");
+                System.exit(0);
+            } else {
+                System.out.println("Wrong option, please choose the right option below :");
+            }
+        }
     }
-    
 }
