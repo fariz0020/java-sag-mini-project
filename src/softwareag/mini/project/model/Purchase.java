e4f0d25930cf0bc762c2deb55efef936a78e780b
fariznurzam <t90fariz@gmail.com>
2018-12-17 11:12:22 +0700

    purchase

---
+++ src/softwareag/mini/project/model/Purchase.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareag.mini.project.model;
+
+/**
+ *
+ * @author fariz.nurzam
+ */
+public class Purchase {
+    
+    private int idPurchase;
+    private String createdAt, updatedAt, deletedAt;
+
+    public Purchase() {
+    }
+
+    public int getIdPurchase() {
+        return idPurchase;
+    }
+
+    public void setIdPurchase(int idPurchase) {
+        this.idPurchase = idPurchase;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(String deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Purchase{" + "idPurchase=" + idPurchase + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + ", deletedAt=" + deletedAt + '}';
+    }
+    
+}
