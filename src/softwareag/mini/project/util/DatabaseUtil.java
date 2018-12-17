2efefe092e4386316da6eac423604ce7bfabdd04
1606858560 <aedwita@gmail.com>
2018-12-17 16:40:50 +0700

    feature/searchCategory

--- src/softwareag/mini/project/util/DatabaseUtil.java
+++ src/softwareag/mini/project/util/DatabaseUtil.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package softwareag.mini.project.util;
 
 import java.sql.Connection;
@@ -50,35 +46,43 @@ public class DatabaseUtil {
             resultSet = statement
                     .executeQuery("select * from products");
             writeResultSetProduct(resultSet);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            close();
+        }
 
-//            // PreparedStatements can use variables and are more efficient
-//            preparedStatement = connect
-//                    .prepareStatement("insert into  feedback.comments values (default, ?, ?, ?, ? , ?, ?)");
-//            // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
-//            // Parameters start with 1
-//            preparedStatement.setString(1, "Test");
-//            preparedStatement.setString(2, "TestEmail");
-//            preparedStatement.setString(3, "TestWebpage");
-//            preparedStatement.setDate(4, new java.sql.Date(2009, 12, 11));
-//            preparedStatement.setString(5, "TestSummary");
-//            preparedStatement.setString(6, "TestComment");
-//            preparedStatement.executeUpdate();
-//
-//            preparedStatement = connect
-//                    .prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from feedback.comments");
-//            resultSet = preparedStatement.executeQuery();
-//            writeResultSet(resultSet);
-//
-//            // Remove again the insert comment
-//            preparedStatement = connect
-//            .prepareStatement("delete from feedback.comments where myuser= ? ; ");
-//            preparedStatement.setString(1, "Test");
-//            preparedStatement.executeUpdate();
-//
-//            resultSet = statement
-//            .executeQuery("select * from feedback.comments");
-//            writeMetaData(resultSet);
+    }
+    
+    public void getProducts() throws Exception {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            Properties properties = new Properties();
+            properties.setProperty("user", Constant.DB_USER);
+            properties.setProperty("password", Constant.DB_PASSWORD);
+            properties.setProperty("useSSL", Constant.DB_USESSL);
+            properties.setProperty("serverTimezone", Constant.DB_SERVERTIMEZONE);
+            
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/"+Constant.DB_SCHEMA, properties);
 
+            // Statements allow to issue SQL queries to the database
+            statement = connect.createStatement();
+            // Result set get the result of the SQL query
+            resultSet = statement
+                    .executeQuery("select * from products where deleted_at is null");
+            
+            while (resultSet.next()) {
+                String user = resultSet.getString("id");
+                String website = resultSet.getString("name");
+             
+                System.out.println("ID: " + user);
+                System.out.println("Category Name: " + website);
+                System.out.println("");
+            }
+            System.out.println("========");
+            
         } catch (Exception e) {
             throw e;
         } finally {
@@ -104,13 +108,9 @@ public class DatabaseUtil {
             statement = connect.createStatement();
             // Result set get the result of the SQL query
             resultSet = statement
-                    .executeQuery("select * from categories");
+                    .executeQuery("select * from categories where deleted_at is null");
 
             while (resultSet.next()) {
-                // It is possible to get the columns via name
-                // also possible to get the columns via the column number
-                // which starts at 1
-                // e.g. resultSet.getSTring(2);
                 String user = resultSet.getString("id");
                 String website = resultSet.getString("name");
              
@@ -184,7 +184,125 @@ public class DatabaseUtil {
             //preparedStatement.setInt(2, product.getIdCategory());
             preparedStatement.executeUpdate();
             
-            System.out.println("New product has been added");
+            System.out.println("New category has been added");
+            
+        } catch (Exception e ) {
+            throw e;
+        } finally {
+            close();
+        }
+    }
+    
+    public void updateProduct(Product product) throws Exception{  
+    	try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            Properties properties = new Properties();
+            properties.setProperty("user", Constant.DB_USER);
+            properties.setProperty("password", Constant.DB_PASSWORD);
+            properties.setProperty("useSSL", Constant.DB_USESSL);
+            properties.setProperty("serverTimezone", Constant.DB_SERVERTIMEZONE);
+            
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/"+Constant.DB_SCHEMA, properties);
+           String sql = String.format("update products set name='%s' where id=%d", product.getName(), product.getId());
+            preparedStatement = connect
+                    .prepareStatement(sql);
+            // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
+            // Parameters start with 1          
+            System.out.println(preparedStatement);            
+            preparedStatement.executeUpdate();
+            
+            System.out.println("New Product has been updated");
+            
+        } catch (Exception e ) {
+            throw e;
+        } finally {
+            close();
+        }
+    }
+    
+    public void updateCategory(Category category) throws Exception{  
+    	try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            Properties properties = new Properties();
+            properties.setProperty("user", Constant.DB_USER);
+            properties.setProperty("password", Constant.DB_PASSWORD);
+            properties.setProperty("useSSL", Constant.DB_USESSL);
+            properties.setProperty("serverTimezone", Constant.DB_SERVERTIMEZONE);
+            
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/"+Constant.DB_SCHEMA, properties);
+           String sql = String.format("update categories set name='%s' where id=%d", category.getName(), category.getId());
+            preparedStatement = connect
+                    .prepareStatement(sql);
+            // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
+            // Parameters start with 1          
+            System.out.println(preparedStatement);            
+            preparedStatement.executeUpdate();
+            
+            System.out.println("New Category has been updated");
+            
+        } catch (Exception e ) {
+            throw e;
+        } finally {
+            close();
+        }
+    }
+    
+    public void deleteProduct(Product product) throws Exception{  
+    	try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            Properties properties = new Properties();
+            properties.setProperty("user", Constant.DB_USER);
+            properties.setProperty("password", Constant.DB_PASSWORD);
+            properties.setProperty("useSSL", Constant.DB_USESSL);
+            properties.setProperty("serverTimezone", Constant.DB_SERVERTIMEZONE);
+            
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/"+Constant.DB_SCHEMA, properties);
+           String sql = String.format("UPDATE products SET deleted_at = NOW() where id='%d'", product.getId());
+           System.out.println(sql);
+            preparedStatement = connect
+                    .prepareStatement(sql);
+            // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
+            // Parameters start with 1          
+            System.out.println(preparedStatement);            
+            preparedStatement.executeUpdate();
+            
+            System.out.println("Product has been deleted");
+            
+        } catch (Exception e ) {
+            throw e;
+        } finally {
+            close();
+        }
+    }
+    
+    public void deleteCategory(Category category) throws Exception{  
+    	try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            Properties properties = new Properties();
+            properties.setProperty("user", Constant.DB_USER);
+            properties.setProperty("password", Constant.DB_PASSWORD);
+            properties.setProperty("useSSL", Constant.DB_USESSL);
+            properties.setProperty("serverTimezone", Constant.DB_SERVERTIMEZONE);
+            
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/"+Constant.DB_SCHEMA, properties);
+           String sql = String.format("UPDATE categories SET deleted_at = NOW() where id='%d'", category.getId());
+           System.out.println(sql);
+            preparedStatement = connect
+                    .prepareStatement(sql);
+            // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
+            // Parameters start with 1          
+            System.out.println(preparedStatement);            
+            preparedStatement.executeUpdate();
+            
+            System.out.println("Category has been deleted");
             
         } catch (Exception e ) {
             throw e;
@@ -300,6 +418,97 @@ public class DatabaseUtil {
                 System.out.println("Name: " + resultSet.getString("name"));
                 System.out.println("Quantity: " + resultSet.getString("quantity"));
                 System.out.println("Category: " + ((resultSet.getString("category_name") != null) ? resultSet.getString("category_name") : "-"));
+                System.out.println("Created at: " + resultSet.getString("created_at"));
+                System.out.println("Last update: " + resultSet.getString("updated_at"));
+                System.out.println("Deleted at: " + ((resultSet.getString("deleted_at") != null) ? resultSet.getString("deleted_at") : "-"));
+                System.out.println("");
+            }
+        }
+    }
+    
+    private void writeResultSetCategory(ResultSet resultSet) throws SQLException {
+        // ResultSet is initially before the first data set
+        while (resultSet.next()) {
+            // It is possible to get the columns via name
+            // also possible to get the columns via the column number
+            // which starts at 1
+            // e.g. resultSet.getSTring(2);
+            String user = resultSet.getString("id");
+            String website = resultSet.getString("name");
+//            String summary = resultSet.getString("quantity");
+            System.out.println("ID: " + user);
+            System.out.println("Name: " + website);
+//            System.out.println("Quantity: " + summary);
+            System.out.println("");
+        }
+        System.out.println("========");
+    }
+    
+//    private void close() {
+//        try {
+//            if (resultSet != null) {
+//                resultSet.close();
+//            }
+//
+//            if (statement != null) {
+//                statement.close();
+//            }
+//
+//            if (connect != null) {
+//                connect.close();
+//            }
+//        } catch (Exception e) {
+//
+//        }
+//    }
+    
+    public void getCategoryBySearch(String keyword, String filter) throws Exception {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String sql = "";
+            
+            Properties properties = new Properties();
+            properties.setProperty("user", Constant.DB_USER);
+            properties.setProperty("password", Constant.DB_PASSWORD);
+            properties.setProperty("useSSL", Constant.DB_USESSL);
+            properties.setProperty("serverTimezone", Constant.DB_SERVERTIMEZONE);
+            
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/"+Constant.DB_SCHEMA, properties);
+            
+                sql = String.format("select count(*) as numberofcategory, categories.name from products left join categories on categories.id=products.id_category where categories.name='%s' group by categories.id;", 
+                        keyword, keyword, keyword);
+                        
+            preparedStatement = connect.prepareStatement(sql);
+            LOGGER.log( Level.INFO, "// query : " + preparedStatement.toString() );
+            resultSet = preparedStatement.executeQuery();
+            
+            int size = 0;
+            if (resultSet != null) 
+            {
+                resultSet.last();
+                size = resultSet.getRow();
+                resultSet.beforeFirst();
+                writeGetCategoryBySearch(resultSet, size);
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            close();
+        }
+    }
+    
+    public void writeGetCategoryBySearch(ResultSet resultSet, int max) throws SQLException {
+        if (resultSet.next() == false ) 
+            System.out.println("The result is empty");
+        else {
+            resultSet.beforeFirst();
+            while (resultSet.next()) {
+                System.out.println("Result " + resultSet.getRow() + " of " + max + " data");
+                System.out.println("ID: " + resultSet.getString("id"));
+                System.out.println("Name: " + resultSet.getString("name"));
+//                System.out.println("Quantity: " + resultSet.getString("quantity"));
+//                System.out.println("Category: " + ((resultSet.getString("category_name") != null) ? resultSet.getString("category_name") : "-"));
                 System.out.println("Created at: " + resultSet.getString("created_at"));
                 System.out.println("Last update: " + resultSet.getString("updated_at"));
                 System.out.println("Deleted at: " + ((resultSet.getString("deleted_at") != null) ? resultSet.getString("deleted_at") : "-"));
